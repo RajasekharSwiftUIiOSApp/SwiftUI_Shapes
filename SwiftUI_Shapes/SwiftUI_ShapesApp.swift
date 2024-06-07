1de748be92fc6062e0b93cc0c61a47440aa3fc33
Raja <Rajasekhar.iosswift@gmail.com>
2024-06-07 12:20:36 +0530

    Initial Commit

---
+++ SwiftUI_Shapes/SwiftUI_ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ShapesApp.swift
+//  SwiftUI_Shapes
+//
+//  Created by rajasekhar pidikiti on 07/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
